4f59fc6444c0699ec2ee8a3a95cb612ba28b0af7
ChaseWelsh <fseusb@gmail.com>
2022-08-08 15:57:06 -0600

    commit tree 6db6be379aff3b89563569c5e5f9f93585c38f35

---
+++ index.js
@@ -0,0 +1,16 @@
+/*
+  index.js
+    1) has a function expression called divide
+    2) divide divides 2000 by 100
+    3) has an arrow function called square
+    4) square arrow function takes one parameter and multiplies it times itself
+    5) has an arrow function called add
+    6) add arrow function takes two parameters and adds them together
+*/
+
+function divide(a, b) {
+    return a/b;
+}
+
+let square = (a)=>{return a*a}
+let add = (a,b)=>{return a+b}
